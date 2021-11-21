ced5e5457a7dc5492574b09984a2d4911395e7e4
ezzeldem <ezzelden06@gmail.com>
2021-11-21 07:37:12 +0200

    add loder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 (function () {
-  
+
+
+  $(".nav_box nav ul li a").click(function () {
+    idName = $(this).attr("data-tab");
+    $(".tab_min_box .tab_box").removeClass("active");
+    $(".tab_min_box .tab_box#" + idName).addClass("active");
+    $(".tab_min_box .tab_box .login_layOut").removeClass("active")
+    setTimeout(function () { 
+      $(".tab_min_box .tab_box.active .login_layOut").addClass("active")
+    }, 800);
+  })
+
 })();
