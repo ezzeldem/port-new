0aa7fb27dd242a1bb10b17964935b2a5368b0dda
ezzeldenWork <ezzelden.work@gmail.com>
2021-11-21 14:00:22 +0200

    add sections && popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 (function () {
-
-
+  // tab toggle
   $(".nav_box nav ul li a").click(function () {
     idName = $(this).attr("data-tab");
     $(".tab_min_box .tab_box").removeClass("active");
     $(".tab_min_box .tab_box#" + idName).addClass("active");
-    $(".tab_min_box .tab_box .login_layOut").removeClass("active")
-    setTimeout(function () { 
-      $(".tab_min_box .tab_box.active .login_layOut").addClass("active")
+    $(".tab_min_box .tab_box .login_layOut").removeClass("active");
+    setTimeout(function () {
+      $(".tab_min_box .tab_box.active .login_layOut").addClass("active");
     }, 800);
-  })
+  });
 
+  // fillter
+
+  if ($(".filter-content").length) {
+    var mixer = mixitup(".filter-content");
+  }
 })();
