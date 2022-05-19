9dbc9228dd5a26e97c5730ad49854dc64e3cb2d9
ezzeldem <ezzelden06@gmail.com>
2022-05-19 10:20:26 +0200

    new Script & Style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 (function () {
   // tab toggle
-  $(".nav_box nav ul li a").click(function () {
-    idName = $(this).attr("data-tab");
-    $(this).addClass("active");
-    $(this).parents("li").siblings().children("a").removeClass("active");
-    $(".tab_min_box .tab_box").removeClass("active");
-    $(".tab_min_box .tab_box#" + idName).addClass("active");
-    $(".tab_min_box .tab_box .login_layOut").removeClass("active");
-    setTimeout(function () {
-      $(".tab_min_box .tab_box.active .login_layOut").addClass("active");
-    }, 800);
-    $("html, body").animate({
-      scrollTop: $('body').offset().top,
+  $(".nav_box nav ul li a").click(function (e) {
+    if (!$(this).hasClass('active')) {
+      idName = $(this).attr("data-tab");
+      $(this).addClass("active");
+      $(this).parents("li").siblings().children("a").removeClass("active");
+      $(".tab_min_box .tab_box").removeClass("active");
+      $(".tab_min_box .tab_box#" + idName).addClass("active");
+      $(".tab_min_box .tab_box .login_layOut").removeClass("active");
+      setTimeout(function () {
+        $(".tab_min_box .tab_box.active .login_layOut").addClass("active");
+      }, 800);
+      $("html, body").animate({
+        scrollTop: $('body').offset().top,
+      }
+        , 0);
     }
-      , 0);
+
+
   });
 
   // fillter
